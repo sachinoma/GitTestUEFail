c80b115fb05a973860e3ee1eb0aaf0e31a14ace0
sachinoma <sachinoma0408@gmail.com>
2023-05-25 12:08:57 +0900

    UeSetting

---
+++ Source/GitTestForFail/GitTestForFail.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GitTestForFail.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GitTestForFail, "GitTestForFail" );
